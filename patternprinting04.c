63867b483560d04bfe93424f5f0739cf5850dbcc
raushan-19 <tsubodh778@gmail.com>
2025-02-28 21:53:19 +0530

    minor change

--- patternprinting04.c
+++ patternprinting04.c
@@ -1,23 +1,43 @@
 // print:-    *****
-//    ****
-//    ***
-//    **
-//    *
+//            ****    
+//            ***
+//            **
+//            *
+
+// type:- 1
+
+    // #include <stdio.h>
+    // int main()
+    // {
+    //     int n;
+    //     printf("Enter number of rows: ");
+    //     scanf("%d", &n);
+
+    //     for (int i = n; i >= 1; i--) // Rows ke liye loop
+    //     {
+    //         for (int j = 1; j <= i; j++) // Har row me stars print karne ka loop
+    //         {
+    //             printf("*");
+    //         }
+    //         printf("\n"); // New line for next row
+    //     }
+    //     return 0;
+    // }
+
+//  type:-2
 
 #include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter number of rows: ");
-    scanf("%d", &n);
-
-    for (int i = n; i >= 1; i--) // Rows ke liye loop
+int n;
+printf("enter number of rows : ");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=n-i+1;j++)
     {
-        for (int j = 1; j <= i; j++) // Har row me stars print karne ka loop
-        {
-            printf("*");
-        }
-        printf("\n"); // New line for next row
+        printf("*");
     }
-    return 0;
+    printf("\n");
+}
 }
